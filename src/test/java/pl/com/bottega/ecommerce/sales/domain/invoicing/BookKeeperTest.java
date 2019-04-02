cef66ae6cf7cffe7d83d91c292808b3d30cd4464
Bogost <bogumilostapiuk@gmail.com>
2019-04-02 12:20:01 +0200

    Dodatkowe zmienne i nadanie konstruktorowi ProductData atrybutu public

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -3,6 +3,8 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.hamcrest.Matchers;
@@ -11,26 +13,48 @@ import org.junit.Test;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
 
+    Date date;
     ClientData klient;
-    List<RequestItem> items;
     BookKeeper bookKeeper;
-    Invoice invoice;
+
+    ProductData apple;
+    ProductData peniciline;
+    ProductData pencil;
+
+    List<RequestItem> noItems;
+    List<RequestItem> someItems;
 
     @Before
     public void setup() {
         klient = new ClientData(Id.generate(), "Klient");
-        items = new ArrayList<>();
         bookKeeper = new BookKeeper();
-        invoice = new Invoice(Id.generate(), klient);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2015);
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DAY_OF_MONTH, 5);
+        date = cal.getTime();
+
+        apple = new ProductData(Id.generate(), new Money(0.5), "apple", ProductType.FOOD, date);
+        peniciline = new ProductData(Id.generate(), new Money(3), "peniciline", ProductType.DRUG, date);
+        pencil = new ProductData(Id.generate(), new Money(1), "pencil", ProductType.STANDARD, date);
+
+        noItems = new ArrayList<>();
+        someItems = new ArrayList<>();
+        someItems.add(new RequestItem(apple, 10, new Money(5)));
+        someItems.add(new RequestItem(peniciline, 10, new Money(30)));
+        someItems.add(new RequestItem(pencil, 10, new Money(10)));
     }
 
     @Test
     public void WithoutProductsThereIsNoProductsOnReturnedInvoice() {
-        int nrOfProductsOnInvoice = bookKeeper.issuance(klient, items)
+        int nrOfProductsOnInvoice = bookKeeper.issuance(klient, noItems)
                                               .getItems()
                                               .size();
         assertThat(nrOfProductsOnInvoice, Matchers.comparesEqualTo(0));
@@ -38,8 +62,9 @@ public class BookKeeperTest {
 
     @Test
     public void WithoutProductsThereIsNothigToPayOnReturnedInvoice() {
-        Money money = bookKeeper.issuance(klient, items)
+        Money money = bookKeeper.issuance(klient, noItems)
                                 .getGros();
         assertThat(money.equals(new Money(0)), Matchers.comparesEqualTo(true));
     }
+
 }
